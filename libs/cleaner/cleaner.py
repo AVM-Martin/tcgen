f8d296c5b8bca77853d46cfe838f14155fcd54ea
AVM.Martin <avmm.martin+github@gmail.com>
2019-07-30 21:59:33 +0700

    Modularize the function

--- libs/cleaner/cleaner.py
+++ libs/cleaner/cleaner.py
@@ -2,16 +2,16 @@ from cleaner import constants
 
 from common import helper
 
+def delete_testcases():
+	helper.delete_file(constants.FILEPATHS["solution_exec"])
+	helper.delete_file(constants.FILEPATHS["runner_exec"])
+	helper.delete_directory(constants.TESTCASE_DIRECTORY)
+
+
+def delete_problem_statement():
+	helper.delete_file(constants.FILEPATHS["pdf"])
+
 
 def cleaner():
-	for _, path in constants.FILEPATHS.items():
-		helper.delete_file(path)
-
-	helper.subprocess_call(
-		args=[
-			"rm",
-			"-rf",
-			constants.TESTCASE_DIRECTORY,
-		],
-		quiet=True,
-	)
+	delete_problem_statement()
+	delete_testcases()
