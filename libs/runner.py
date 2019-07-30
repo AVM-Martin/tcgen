809fe74e60e2f7e1c7a9ca467bceea577b6ea674
AVM.Martin <avmm.martin+github@gmail.com>
2019-07-30 22:00:27 +0700

    Change the library name based on previous commit

--- libs/runner.py
+++ libs/runner.py
@@ -4,14 +4,14 @@ from common import constants
 
 from compressed_problem import compressed_problem
 
-from generate_problem_description import generate_problem_description
+from generate_problem_statement import generate_problem_statement
 
 from generate_testcases import generate_testcases
 
 
 if __name__ == "__main__":
 	try:
-		generate_problem_description.generate_problem_description()
+		generate_problem_statement.generate_problem_statement()
 		generate_testcases.generate_testcases()
 		compressed_problem.compressed_problem()
 		cleaner.cleaner()
